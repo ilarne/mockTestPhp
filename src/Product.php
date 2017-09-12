672756b227f3f09a285e7da1c150e96ceb9d46f8
Paula Muldoon <paula.muldoon@kurtgeiger.com>
2017-09-12 16:16:04 +0100

    part-way through requirement 3

--- src/Product.php
+++ src/Product.php
@@ -8,4 +8,25 @@ class Product
     {
         $this->productService = $productService;
     }
+
+    public function totalPrice()
+    {
+        return $this->productService->getPrice() * 1.2;
+    }
+
+    public function showDescription()
+    {
+        if ($this->productService->getDescription()) {
+            return $this->productService->getDescription();
+        }
+        else {
+            return $this->productService->getName();
+        }
+    }
+
+    public function getStock($productId)
+    {
+        if ($this->productService->returnStock() > 5)
+            return 'In Stock';
+    }
 }
--- tests/ProductTest.php
+++ tests/ProductTest.php
@@ -1,8 +1,67 @@
 <?php
+use PHPUnit\Framework\TestCase;
 
 require (__DIR__ . '/../src/Product.php');
 
-class ProductTest extends PHPUnit_Framework_TestCase
+class ProductTest extends TestCase
 {
+    public function testTotalPrice()
+    {
+       $priceMock = $this->getMockBuilder('ProductService')
+            ->setMethods(array('getPrice'))->getMock();
 
+       $priceMock->method('getPrice')
+           ->willReturn(10);
+
+        $product = new Product($priceMock);
+
+        $this->assertEquals(12, $product->totalPrice());
+    }
+
+    public function testProductDescriptionReturned()
+    {
+        $descriptionMock = $this->getMockBuilder('ProductService')
+            ->setMethods(array('getDescription'))->getMock();
+
+        $descriptionMock->method('getDescription')
+            ->willReturn('expensive');
+
+        $product = new Product($descriptionMock);
+
+        $this->assertEquals('expensive', $product->showDescription());
+    }
+
+    public function testNoProductDescriptionReturned()
+    {
+        $descriptionMock = $this->getMockBuilder('ProductService')
+            ->setMethods(array('getDescription', 'getName'))->getMock();
+
+        $descriptionMock->method('getDescription')
+            ->willReturn(null);
+
+        $descriptionMock->method('getName')
+            ->willReturn('iPhone');
+
+        $product = new Product($descriptionMock);
+
+        $this->assertEquals('iPhone', $product->showDescription());
+    }
+
+    public function testReturnsInStock()
+    {
+        $productIdMock = $this->getMockBuilder('ProductService')
+            ->setMethods(array('returnStock'))->getMock();
+
+        $productIdMock->method('returnStock')
+            ->willReturn(6);
+
+        $product = new Product($productIdMock);
+
+        $this->assertEquals('In Stock', $product->getStock($productIdMock));
+    }
+
+    public function testReturnsLowStock()
+    {
+        
+    }
 }
